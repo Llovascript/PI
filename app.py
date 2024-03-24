956cce13b0b0452a23354190156995f734d24e0e
Llovascript <157436170+Llovascript@users.noreply.github.com>
2024-03-23 20:35:32 -0600

    intento 1

--- app.py
+++ app.py
@@ -15,6 +15,31 @@ mysql=MySQL(app)
 def home():
     return render_template('index.html')
 
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+#func de login
+@app.route('/acceso-login', methods=["GET","POST"])
+def login():
+    
+    if request.method == 'POST' and 'textCorreo' in request.form and 'txtPassword':
+        _correo = request.form['txtCorreo']
+        _password = request.form['txtPassword']
+        
+        cur=mysql.connection.cursor()
+        cur.execute('SELECT * FROM usuarios WHERE correo = %s AND password = %s', (_correo,_password,))
+        account = cur.fetchone()
+        
+        if account:
+            session['logueado'] = True
+            session['id'] = account['id']
+            
+            return render_template('admin.html')
+        else:
+            return render_template('index.html')
+
+
 if __name__ == '__main__':
-    app.secret_key='acm1pt'
+    app.secret_key="acm1pt"
     app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
